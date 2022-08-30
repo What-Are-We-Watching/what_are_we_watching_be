c6c128fac343fdc9e81e2a173e2fea77ba0ba69f
Chris Kjolhede <ckjolhede@gmail.com>
2022-08-30 17:32:22 -0600

    graphql_initialized, db seeded

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :event_movies
-  resources :movies
-  resources :events
-  resources :users
+  post "/graphql", to: "graphql#execute"
+  # resources :event_movies
+  # resources :movies
+  # resources :events
+  # resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
