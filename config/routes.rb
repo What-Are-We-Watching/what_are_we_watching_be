a0ce8e59b0d6fd367cc374f894405b74ef00ef7c
Eric Espindola <eric_e_2012@hotmail.com>
2022-09-01 14:50:23 -0700

    Merge branch 'event_graphql' of github.com:erock02/what_are_we_watching_be into event_graphql

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: "graphql#execute"
+    mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: "/graphql"
   end
 
 
@@ -15,5 +15,5 @@ end
   # resources :movies
   # resources :events
   # resources :users
-  # For details on the DSL available within this file, see 
+  # For details on the DSL available within this file, see
   # http://guides.rubyonrails.org/routing.html
