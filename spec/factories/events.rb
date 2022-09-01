02f90a1e9546b6308c213c8e71e1bfcf6ae36223
Eric Espindola <eric_e_2012@hotmail.com>
2022-09-01 11:03:41 -0700

    updated event factory for easier event instance creation

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :event do
-    date { "2022-08-30 08:55:30" }
-    name { "MyString" }
-    users { nil }
-    movie_selection_id { 1 }
+    date { Faker::Date.between(from: 2.days.ago, to: Date.today) }
+    name { Faker::Name.name }
+    movie_selection_id {Faker::Number.within(range: 1..5)}
+
+    association :user, factory: :user
   end
 end
