d101dc9c3ac7f0af8f53d43ba606314868d3e076
Eric Espindola <eric_e_2012@hotmail.com>
2022-09-01 11:05:13 -0700

    update pluaral foreign keys to singular

--- db/migrate/20220830145530_create_events.rb
+++ db/migrate/20220830145530_create_events.rb
@@ -3,7 +3,7 @@ class CreateEvents < ActiveRecord::Migration[5.2]
     create_table :events do |t|
       t.datetime :date
       t.string :name
-      t.references :users, foreign_key: true
+      t.references :user, foreign_key: true
       t.integer :movie_selection_id
       t.integer :status, default: 0
       t.integer :guest_id
--- db/migrate/20220830145748_create_event_movies.rb
+++ db/migrate/20220830145748_create_event_movies.rb
@@ -1,8 +1,8 @@
 class CreateEventMovies < ActiveRecord::Migration[5.2]
   def change
     create_table :event_movies do |t|
-      t.references :events, foreign_key: true
-      t.references :movies, foreign_key: true
+      t.references :event, foreign_key: true
+      t.references :movie, foreign_key: true
       t.integer :vote
 
       t.timestamps
