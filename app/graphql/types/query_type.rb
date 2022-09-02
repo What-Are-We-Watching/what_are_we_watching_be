502715d156c91046fa850f65f6f946dbcc57889e
Drew Proebstel <d.proebstel@gmail.com>
2022-09-01 21:32:58 -0700

    wip

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,16 +1,20 @@
 module Types
   class QueryType < Types::BaseObject
     # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
-    include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
 
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
     # TODO: remove me
-    field :event_movies, [Types::EventMovieType], null: false do
-      argument :user_id, ID1
-    end
+    # field :event_movies, [Types::EventMovieType], null: false do
+    #   argument :user_id, user_id, required: true
+    #   argument :event_id, event_id, required: true
+    # end
+    #
+    # def event_movies(user_id, event_id)
+    #   binding.pry
+    # end
 
     field :events,
     [Types::EventType],
