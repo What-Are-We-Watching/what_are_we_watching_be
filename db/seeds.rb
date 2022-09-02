954c3437f5361f662f62301294924b7ff0d6180c
Drew Proebstel <d.proebstel@gmail.com>
2022-09-01 20:10:37 -0700

    wip

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# 5.times do |i|
-#   user = User.create(name: Faker::Name.name)
-#   event = Event.create(name: Faker::Name.name, users_id: user.id, date: Faker::Date.in_date_period )
-#   movie = Movie.create!(title: Faker::Movie.title, overview: Faker::Movie.quote, moviedb_id:(i + 2 + 1), image: Faker::LoremFlickr.image)
-#   event_movie = EventMovie.create(movies_id: movie.id, events_id: event.id, vote: 0)
-# end
+5.times do |i|
+  user = User.create(name: Faker::Name.name)
+  event = Event.create(name: Faker::Name.name, users_id: user.id, date: Faker::Date.in_date_period )
+  movie = Movie.create!(title: Faker::Movie.title, overview: Faker::Movie.quote, moviedb_id:(i + 2 + 1), image: Faker::LoremFlickr.image)
+  event_movie = EventMovie.create(movies_id: movie.id, events_id: event.id, vote: 0)
+end
 
 20.times do |i|
   movie = MovieFacade.new
