cce6b801b06ae50b4f3d6aaa54dec14ca9e56c3a
Drew Proebstel <d.proebstel@gmail.com>
2022-09-02 19:23:26 -0700

    date changed to date time instead of int

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,11 +20,11 @@ user_3 = User.create(name: "user_3")
 user_4 = User.create(name: "user_4")
 user_5 = User.create(name: "user_5")
 
-event_1 = Event.create!(date: "6/23/2023", name: "event_1", user_id: user_1.id, guest_id: user_4.id)
-event_2 = Event.create!(name: "event_2", user_id: user_4.id, guest_id: user_3.id, date: "6/23/2023" )
-event_3 = Event.create!(name: "event_3", user_id: user_3.id, guest_id: user_2.id, date: "6/23/2023" )
-event_4 = Event.create!(name: "event_4", user_id: user_2.id, guest_id: user_5.id, date: "6/23/2023" )
-event_5 = Event.create!(name: "event_5", user_id: user_5.id, guest_id: user_1.id, date: "6/23/2023" )
+event_1 = Event.create!(name: "event_1", user_id: user_1.id, guest_id: user_4.id, date: DateTime.new(2023,6,23,12) )
+event_2 = Event.create!(name: "event_2", user_id: user_4.id, guest_id: user_3.id, date: DateTime.new(2023,6,23,12) )
+event_3 = Event.create!(name: "event_3", user_id: user_3.id, guest_id: user_2.id, date: DateTime.new(2023,6,23,12) )
+event_4 = Event.create!(name: "event_4", user_id: user_2.id, guest_id: user_5.id, date: DateTime.new(2023,6,23,12) )
+event_5 = Event.create!(name: "event_5", user_id: user_5.id, guest_id: user_1.id, date: DateTime.new(2023,6,23,12) )
 
 # 5.times do |i|
 #   user = User.create!(name: Faker::Name.name)
