22cb3c141f94995320cd768a162f342a45384186
Chris Kjolhede <ckjolhede@gmail.com>
2022-09-05 12:25:04 -0600

    create join_event mutation; ;

---
+++ app/graphql/mutations/join_event.rb
@@ -0,0 +1,25 @@
+class Mutations::JoinEvent < Mutations::BaseMutation 
+  argument :event_id, Integer, required: true
+  argument :user_id, Integer, required: true
+
+  field :event, Types::EventType, null: false
+  field :errors, [String], null: false
+  
+  def resolve(event_id:, user_id:)
+    event=Event.find(event_id)
+    event.update(guest_id: user_id)
+
+    if (event.save)
+      {
+        event: event,
+        errors: []
+      }
+    else
+      {
+        event: nil,
+        errors: event.errors.full_messages
+      }
+    end
+  end
+
+end
