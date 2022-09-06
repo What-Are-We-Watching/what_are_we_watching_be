5a0b544a5aa91d24d2d3df475955b0f03ccd4c1e
Drew Proebstel <d.proebstel@gmail.com>
2022-09-06 15:31:40 -0700

    add facade spec

--- spec/facades/movie_facade_spec.rb
+++ spec/facades/movie_facade_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe "MovieFacade", type: :facade do
+RSpec.describe "MovieFacade" do
   describe "get movies" do
-    xit "returns a set of movies as a hash" do
-      movies = MovieFacade.top_movie_seeding(1)
+    it "returns a set of movies as a hash" do
+      facade = MovieFacade.new
+      movies = facade.top_movie_seeding(1)
 
-      # expect(movies).to be_a(Movie)
-      expect(movies).to be_a(Hash)
+
+
+      expect(movies).to be_a(Array)
+      expect(movies[3]).to be_a(Hash)
+      expect(movies[3][:title]).to be_a(String)
+      expect(movies[1][:moviedb_id]).to be_a(Integer)
+      expect(movies[0][:image]).to be_a(String)
     end
   end
 end
