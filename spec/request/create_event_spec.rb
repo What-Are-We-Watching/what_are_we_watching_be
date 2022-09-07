3ffd2e233370508bbbe1f17e8fbb7c87a5b87ff7
Drew Proebstel <d.proebstel@gmail.com>
2022-09-07 15:11:21 -0700

    create event sad path

--- spec/request/create_event_spec.rb
+++ spec/request/create_event_spec.rb
@@ -8,11 +8,32 @@ RSpec.describe Mutations::CreateEvent, type: :request do
       post '/graphql', params: { query: query }
       expect(Event.count).to eq(count + 1)
     end
+
+
+    it 'does not creates an event with bad input' do
+      @user_1 = User.create(name: "user_1")
+      count = Event.count
+      post '/graphql', params: { query: badquery }
+      expect(Event.count).to_not eq(count + 1)
+    end
   end
 
   def query
     <<~GQL
     mutation {createEvent(input:{date: "2022-09-04", name: "newevent", userId: #{@user_1.id}}){
+       event {
+         date
+         name
+         userId
+       }
+    }
+     }
+    GQL
+  end
+
+  def badquery
+    <<~GQL
+    mutation {createEvent(input:{date: "2022-09-04", userId: #{@user_1.id}}){
        event {
          date
          name
